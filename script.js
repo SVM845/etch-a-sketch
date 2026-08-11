d2855c32e4772af2b5043f35157573a6514fdf2b
Hesam Sormeyli <hesam1385hs@gmail.com>
2026-08-11 17:06:29 +0330

    size button (incomplete)

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const gridContainer = document.querySelector('#gridContainer');
+const sizeButton = document.querySelector('#sizeButton');
+const resetButton = document.querySelector('#resetButton');
 
-for (let i = 1; i <= 256; i++) {
-    const square = document.createElement('div');
-    square.classList.add("squares");
-    gridContainer.appendChild(square);
-    square.addEventListener("mouseenter", (e) => {
-        e.currentTarget.style = `
+
+
+sizeButton.addEventListener("click", (e) => {
+    let size = prompt("Squares Per Side? (side*side squares)");
+    let sqrSize = size*size;
+    for (let i = 1; i <= sqrSize; i++) {
+        const square = document.createElement('div');
+        square.classList.add("squares");
+        gridContainer.appendChild(square);
+        square.addEventListener("mouseenter", (e) => {
+            e.currentTarget.style = `
         background-color: red`
-})
-}
+        })
+    }
 
+})
