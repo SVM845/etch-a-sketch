2a38797e84f517ada6ddc5996555dcd61a979cf6
Hesam Sormeyli <hesam1385hs@gmail.com>
2026-08-12 14:04:27 +0330

    size button
    Done

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let sqrSize = 0;
 let calcWidth = 0;
 
 sizeButton.addEventListener("click", (e) => {
-    
+    gridContainer.innerHTML = "";
     let size = prompt("Squares Per Side? (side*side squares)");
     if (size > 100) {
         alert("Limit Is 100");
@@ -14,20 +14,22 @@ sizeButton.addEventListener("click", (e) => {
     } else {
         calcWidth = `${(1 / size) * 100}%`;
         sqrSize = size * size;
+        makeGrid(sqrSize);
     }
 
+    function makeGrid(number) {
+        for (let i = 1; i <= number; i++) {
+            const square = document.createElement('div');
+            square.classList.add("squares");
+            square.style.width = calcWidth;
+            gridContainer.appendChild(square);
+            // square.addEventListener("mouseenter", (e) => {
+            //     e.currentTarget.style = `
+            // background-color: red`
+            // })
+        }
 
-    for (let i = 1; i <= sqrSize; i++) {
-        const square = document.createElement('div');
-        square.classList.add("squares");
-        square.style.width = calcWidth;
-        gridContainer.appendChild(square);
-        // square.addEventListener("mouseenter", (e) => {
-        //     e.currentTarget.style = `
-        // background-color: red`
-        // })
     }
-    const count = gridContainer.querySelectorAll('div').length;
-    console.log(count);
+
 })
 
