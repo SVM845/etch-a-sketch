397927ad3776731b05a06c8e6cfde86a7910f455
Hesam Sormeyli <hesam1385hs@gmail.com>
2026-08-12 13:55:33 +0330

    size button with dynamic grid size
    still incomplete

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 const gridContainer = document.querySelector('#gridContainer');
 const sizeButton = document.querySelector('#sizeButton');
 const resetButton = document.querySelector('#resetButton');
-
-
+let sqrSize = 0;
+let calcWidth = 0;
 
 sizeButton.addEventListener("click", (e) => {
+    
     let size = prompt("Squares Per Side? (side*side squares)");
-    let sqrSize = size*size;
+    if (size > 100) {
+        alert("Limit Is 100");
+        size = 0;
+        size = prompt("Squares Per Side? (side*side squares)");
+    } else {
+        calcWidth = `${(1 / size) * 100}%`;
+        sqrSize = size * size;
+    }
+
+
     for (let i = 1; i <= sqrSize; i++) {
         const square = document.createElement('div');
         square.classList.add("squares");
+        square.style.width = calcWidth;
         gridContainer.appendChild(square);
-        square.addEventListener("mouseenter", (e) => {
-            e.currentTarget.style = `
-        background-color: red`
-        })
+        // square.addEventListener("mouseenter", (e) => {
+        //     e.currentTarget.style = `
+        // background-color: red`
+        // })
     }
-
+    const count = gridContainer.querySelectorAll('div').length;
+    console.log(count);
 })
+
